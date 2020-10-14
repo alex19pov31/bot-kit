c38e68b7510019c9cdfc83f8a0a854678ba8a3d9
Alexander Nesterov <alex19pov31@gmail.com>
2020-10-14 16:18:15 +0300

    init repo

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import bot_kit
+
+install_requires = []
+with open('requirements.txt') as f:
+    install_requires = f.read().replace('==', '>=').split()
+
+setup(
+    name="bot_kit",
+    version=proxy_client.__version__,
+    packages=find_packages(),
+    author="Alexander Nesterov",
+    author_email="alex19pov31@gmail.com",
+    license="MIT",
+    install_requires=install_requires,
+    python_requires='>=3.6',
+)
